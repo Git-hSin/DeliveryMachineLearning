cdb8f56ebf56d1b26633d60a3117fb4a7d7721d1
Git-hSin <dustries.hsin@gmail.com>
2019-05-26 17:21:35 -0700

    clean excel and make first df

--- Data/model.py
+++ Data/model.py
@@ -19,13 +19,32 @@ import plotly.graph_objs as go
 
 filename = 'data.xlsx'
 
-
+def one_hot(df, cols):
+    """
+    @param df pandas DataFrame
+    @param cols a list of columns to encode 
+    @return a DataFrame with one-hot encoding
+    """
+    for each in cols:
+        dummies = pd.get_dummies(df[each], prefix=each, drop_first=False)
+        df = pd.concat([df, dummies], axis=1)
+    return df
 
 df_deliver_from = pd.read_excel(filename, sheet_name='from')  # Shape (32776, 14)
-df_deliver_to = pd.read_excel(filename, sheet_name='to') # Shape (657, 14)
+df_deliver_to = pd.read_excel(filename, sheet_name='to') # Shape (657, 14) 339/657 missing some data
 
 df_delivery = df_deliver_from.merge(df_deliver_to, how='inner', left_on='Account', right_on='Account') # Shape (40619, 27)
 
-train_set, test_set = train_test_split(df_delivery, test_size=0.2, random_state=42)
+
+numeric_variables= ['Date', 'PlanArrival', 'ActualArrival', 'Miles', 'PlanVsActual', 'TravelTime', 'HasNAs']
+categorical_variables = [ 'Day', 'Week', 'Month', 'Quarter', 'Driver', 'VehicleID',
+       'Account',  'Supervisor', 'Shift',
+       'ShiftHour', 'DepartureDoor', 'AccountName', 'Address',
+       'City', 'State', 'ZipCode']
+
+
+df_ML = one_hot(df_delivery, categorical_variables)
+
+train_set, test_set = train_test_split(df_ML, test_size=0.2, random_state=42)
 
 df_copy = train_set.copy()
